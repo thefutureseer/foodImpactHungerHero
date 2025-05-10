b846aa070d15b975b27c6f40b238e4884ad61e7d
thefutureseer <danielharrislimos@outlook.com>
2025-05-10 04:01:49 -0700

    fix: add typscript more fully in the api

--- next-client/src/pages/api/food.ts
+++ next-client/src/pages/api/food.ts
@@ -2,15 +2,15 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { FoodData } from '@/lib/interfaces/FoodImpact';
 import foodData from '@/lib/data/foodData';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextApiRequest, res: NextApiResponse<FoodData | {error:string}>) {
   try {
     // Check if foodData exists
     if (!foodData) {
       throw new Error("Food data not found");
-    }
-
+    };
+    const data:FoodData = foodData;
     // Return the foodData as JSON
-    res.status(200).json(foodData);
+    res.status(200).json(data);
   } catch (error: any) {
     // Handle errors
     console.error("Error fetching food data:", error.message);
